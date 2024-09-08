c9c030383071b6491cee6950830efe0afd465c29
EliasZare <1EliasZare1@gmail.com>
2024-09-08 22:52:12 +0430

    Add project files.

---
+++ 03-ObjectOrientedProgrammingExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using Teacher;
+
+Console.WriteLine("Hello, World!");
+//I can creaet a object of teacher by constructor
+Teacher.Teacher teacher1 = new Teacher.Teacher(1, "Abbas", "Math", 1);
+teacher1.CalculateSalary();
+//Naming Parameter 
+teacher1.CalculateSalary(workHistoryYear: 7, mejor: "Chemestry");
+Console.WriteLine($"Hey {teacher1.Name}, your salary is {teacher1.Salary}$");
+Console.WriteLine($"The {Teacher.Teacher.SchoolName}School salary is {Teacher.Teacher.GetSchoolSalary()}");
+
+
+
+int argNum1 = 0;
+int argNum2 = 0;
+int argNum3 = 0;
+RefParameter(ref argNum1);
+OutParameter(out argNum2);
+InParameter(in argNum3);
+int[] intArry = { 1, 2, 3, 4, 5, 6, 7 };
+ParamsParameter(intArry);
+Console.WriteLine(argNum1);
+Console.WriteLine(argNum2);
+Console.WriteLine(argNum3);
+Console.ReadKey();
+
+
+
+
+void RefParameter(ref int setNum)
+{
+    setNum = 10;
+}
+
+
+void OutParameter(out int setNum)
+{
+    //Console.WriteLine("setNum in Out: " + setNum);
+
+    setNum = 10;
+}
+void InParameter(in int setNum/*readonly*/)
+{
+    //setNum = 10;//Error, can not change the prameter value  
+}
+
+void ParamsParameter(params int[] param)
+{
+    Console.WriteLine(param[0]);
+}
---
+++ Teacher/Teacher.cs
@@ -0,0 +1,103 @@
+﻿namespace Teacher
+{
+    public class Teacher
+    {
+        //Instance fields 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //Readonly fields
+        public readonly int WorkHistoryYear;
+        public string Mejor { get; set; }
+        public double Salary { get; set; }
+        //Static fields
+        public static string SchoolName { get; set; }
+        //Constent fields
+        public const double BaseSalary = 10000;
+
+
+
+        public Teacher(long id, string name, string mejor)
+        {
+            this.Id = Id;
+            this.Name = name;
+            this.Mejor = mejor;
+
+        }
+        public Teacher(long id, string name, string mejor, int workHistoryYear)
+        {
+            //this keyword
+            this.Id = Id;
+            this.Name = name;
+            this.Mejor = mejor;
+            this.WorkHistoryYear = workHistoryYear;
+        }
+        //Constructor Overloading
+        public Teacher(long id, string name, string mejor, string schoolName)
+        {
+            this.Id = Id;
+            this.Name = name;
+            this.Mejor = mejor;
+        }
+        //Static Constructor
+        static Teacher()
+        {
+            SchoolName = "NamAvar";
+        }
+
+
+        public void CalculateSalary()
+        {
+            double salary = 0.0;
+
+            double cal = (BaseSalary * this.WorkHistoryYear) / 100;
+            salary = BaseSalary + cal;
+
+            switch (this.Mejor)
+            {
+                case "Math":
+                    salary = salary * 4;
+                    break;
+                case "Physics":
+                    salary = salary * 3;
+                    break;
+                case "Chemestry":
+                    salary = salary * 2;
+                    break;
+            }
+            this.Salary = salary;
+        }
+        //Method Overloading
+        public double CalculateSalary(int workHistoryYear, string mejor)
+        {
+            double salary = 0.0;
+
+            double cal = (BaseSalary * workHistoryYear) / 100;
+            salary = BaseSalary + cal;
+
+            switch (mejor)
+            {
+                case "Math":
+                    salary = salary * 4;
+                    break;
+                case "Physics":
+                    salary = salary * 3;
+                    break;
+                case "Chemestry":
+                    salary = salary * 2;
+                    break;
+            }
+            return salary;
+        }
+        //Static Method
+        public static string GetSchoolSalary()
+        {
+            double length = SchoolName.Length;
+            //Local fanction
+            double CalculateSalary(double length,double baseSalary)
+            {
+               return  BaseSalary * length;
+            }
+            return CalculateSalary(length, BaseSalary).ToString();
+        }
+    }
+}
